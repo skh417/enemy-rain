593200e6a88b3e5873f47132830f2334bdc0c7cb
Simple key <skh417@gmail.com>
2020-08-18 15:53:00 +0900

    refactoring

--- js/ghost.js
+++ js/ghost.js
@@ -1,88 +1,50 @@
+const background = document.getElementsByClassName("bgContainer")[0];
 
-setInterval(createGhost, 3000); // 고스트 랜덤 출현
-function createGhost(){
-    const ghostDiv = document.createElement('div'); // 고스트 div 태그 생성
-    document.getElementsByClassName('bgContainer')[0].appendChild(ghostDiv);//부모태그에 고스트 div태그 삽입
-    ghostDiv.classList.add('ghost'); 
-    ghostDiv.style.left = (Math.random() * 750) + 'px'
-    //console.log(ghostDiv.getBoundingClientRect().top);
+setInterval(createGhost, 3000); // 고스트 출현 반복시간 설정
+setInterval(fallingGhost, 10); // 고스트 내려오는 속도 설정
+
+// setInterval에 첫번째 인자로 들어갈 고스트 생성 함수 설정
+function createGhost() {
+  const ghostDiv = document.createElement("div"); // 고스트 div 태그 생성
+  document.getElementsByClassName("bgContainer")[0].appendChild(ghostDiv); //부모태그에 고스트 div태그 삽입
+  ghostDiv.className = "ghost"; // ghost라는 class 명을 부여한다.
+  //ghostDiv.classList.add("ghost"); Element.classList.add()도 클래스명을 부여할 수 있는 방법 .remove()로 삭제도 가능
+  ghostDiv.style.left = Math.random() * 750 + "px"; // 고스트의 출현 위치를 랜덤으로 설정
 }
-setInterval(fallingGhost, 10) // 고스트 내려오는 함수 + 땅에 도착하거나 히어로에 부딛히면 고스트가 죽는 함수
-function fallingGhost(){
-    const ghost = document.getElementsByClassName('ghost');//고스트 접근
-    for(let i = 0; i < ghost.length; i++){
-        const goDown = ghost[i].offsetTop + 2;
-        if(ghost[i].offsetTop === 550){
-            // let bg = document.getElementsByClassName('bgContainer')[0];
-            // let ghostDead = document.getElementsByClassName('ghostDead')[0];       
-            ghost[i].style.backgroundPosition = '45px';
-            ghost[i].className = 'ghostDead'
-            // setTimeout(() => {
-            //     bg.removeChild(ghostDead)
-            // },1000);   
-            setTimeout(() => {
-                removeGhost();
-            }, 2000);
-        } else if(ghost[i].offsetTop === 490 && ghost[i].offsetLeft > hero.offsetLeft - 45 && ghost[i].offsetLeft < hero.offsetLeft+35){
-            ghost[i].style.backgroundPosition = '45px';
-            ghost[i].className = 'ghostDead';
-            setTimeout(() => {
-                    removeGhost();
-                }, 2000);
-        } else {
-            ghost[i].style.top = goDown + 'px';
-        }
+
+function fallingGhost() {
+  const ghost = document.getElementsByClassName("ghost"); //고스트 접근
+  for (let i = 0; i < ghost.length; i++) {
+    const goDown = ghost[i].offsetTop + 2;
+    if (ghost[i].offsetTop > 545) {
+      // ghost가 바닥까지 도달한 경우 ? 증발..
+      ghost[i].className = "ghostAlive";
+      setTimeout(() => {
+        removeGhost(false);
+      }, 500);
+    } else if (
+      // ghost가 hero와 접촉한 경우 ? 사망..
+      ghost[i].offsetTop + 30 > hero.offsetTop &&
+      ghost[i].offsetTop + 30 < hero.offsetTop + 54 &&
+      ghost[i].offsetLeft > hero.offsetLeft - 30 &&
+      ghost[i].offsetLeft < hero.offsetLeft + 30
+    ) {
+      ghost[i].style.backgroundPosition = "45px";
+      ghost[i].className = "ghostDead";
+      setTimeout(() => {
+        removeGhost(true);
+      }, 2000);
+    } else {
+      // ghost의 하강
+      ghost[i].style.top = goDown + "px";
     }
+  }
 }
 
-function removeGhost(){
-    let ghostDead = document.getElementsByClassName('ghostDead')[0];
-    let bg = document.getElementsByClassName('bgContainer')[0];
-    bg.removeChild(ghostDead);
+function removeGhost(isGhostDead) {
+  let ghostDead = document.getElementsByClassName("ghostDead")[0];
+  let ghostAttack = document.getElementsByClassName("ghostAlive")[0];
+  isGhostDead
+    ? background.removeChild(ghostDead)
+    : background.removeChild(ghostAttack);
 }
-
-
-
-
-
-
-
-//setInterval(showGhost , 4000);
-
-// function showGhost(){
-//     if(true){
-//         const ghostDiv =  document.createElement('div'); // 유령이 들어갈 div태그 생성
-//         const addGhostDiv = document.getElementsByClassName('bgContainer')[0].appendChild(ghostDiv);// 유령 div를 부모 div태그에 appendChild 하기
-//         const random600 = Math.random()*750 ; // 0~600에서 소숫점 포함한 랜덤 숫자
-//         const randomNum = Math.floor(random600); // 0~600에서 소숫점 뺀 랜덤 숫자
-//         addGhostDiv;
-//         ghostDiv.classList.add('ghost');
-//         ghostDiv.style.left = randomNum + 'px'
-//         setInterval (function ghostRain(){
-//             const ghost =  document.getElementsByClassName('ghost');
-//             ghost[0].style.backgroundPositon = '45px'
-//             for(let i = 0; i < ghost.length; i++){
-//                 const goDown = ghost[i].offsetTop;
-//                 if(ghost[i].offsetBottom === '0px'){
-//                     ghost[i].style.backgroundPosition = '45px'
-//                 } else {
-//                     ghost[i].style.top = (goDown + 20)+ 'px';
-//                 }
-//             }
-//         }, 2000)
-        
-//     }
-// }
-
-
-// function ghostRain(){
-//     const ghost =  document.getElementsByClassName('ghost')[i];
-//     for(let i = 0; i < ghost.length; i++){
-//         ghost.style.top = ghost.offsetTop + 'px';
-//     }
-// }
-
-
-
-
-
--- js/hero.js
+++ js/hero.js
@@ -1,36 +1,26 @@
 const hero = document.getElementsByClassName("hero")[0]; // 히어로
-const cssOfHero = window.getComputedStyle(hero); // .hero의 css에 접근
-let leftValue = cssOfHero.left; // .here의 css 효과중 left의 값
-let leftValueWithoutPx = parseInt(leftValue); // .hero의 css중 left의 값에서 px제거한 숫자
+// const cssOfHero = window.getComputedStyle(hero); // .hero의 css에 접근
+// let leftValue = cssOfHero.left; // .hero의 css 효과중 left의 값
+// let leftValueWithoutPx = parseInt(leftValue); // .hero의 css중 left의 값에서 px제거한 숫자
+
+// 바닥에 있을때 hero offsetTop = 546 , 맨 위에 있을땐 6
 
 window.addEventListener("keydown", move);
 
 function move(e) {
-  if ((e.key === "a" || e.key === "ㅁ") && hero.offsetLeft > 0) {
+  console.log(hero.offsetLeft);
+  //   console.log(e.keyCode); // 콘솔창에서 keyCode 확인하기 ;
+  if (e.keyCode === 37 && hero.offsetLeft > 0) {
     hero.style.left = hero.offsetLeft - 20 + "px";
     hero.style.backgroundPosition = "70px";
-  } else if ((e.key === "d" || e.key === "ㅇ") && hero.offsetLeft < 770) {
+  } else if (e.keyCode === 39 && hero.offsetLeft < 760) {
     hero.style.left = hero.offsetLeft + 20 + "px";
     hero.style.backgroundPosition = "35px";
-  } else if (e.key === "s" || e.key === "ㄴ") {
+  } else if (e.keyCode === 40 && hero.offsetTop < 545) {
+    hero.style.top = hero.offsetTop + 20 + "px";
     hero.style.backgroundPosition = "0px";
-  } else if (e.key === "w" || e.key === "ㅈ") {
+  } else if (e.keyCode === 38 && hero.offsetTop > 10) {
+    hero.style.top = hero.offsetTop - 20 + "px";
     hero.style.backgroundPosition = "105px";
   }
 }
-
-// function move(e){
-//         if(e.key === 'a' || e.key === 'ㅁ'){
-//             hero.style.backgroundPosition = '70px'
-//             let toLeft = leftValueWithoutPx -= 5;
-//             hero.style.left = toLeft+'px';
-//         } else if(e.key === 'd' || e.key === 'ㅇ'){
-//             hero.style.backgroundPosition = '35px'
-//             let toRight = leftValueWithoutPx += 5;
-//             hero.style.left = toRight+'px';
-//         } else if(e.key === 's' || e.key ==='ㄴ'){
-//             hero.style.backgroundPosition = '0px'
-//         } else if(e.key === 'w' || e.key === 'ㅈ'){
-//             hero.style.backgroundPosition = '105px'
-//         }
-// }
