52cd6ccb102afebca71a2f4d842294837a146ca7
Simple key <skh417@gmail.com>
2020-08-18 13:15:10 +0900

    modified hero.js

--- js/hero.js
+++ js/hero.js
@@ -1,23 +1,23 @@
-const hero = document.getElementsByClassName('hero')[0] // 히어로
+const hero = document.getElementsByClassName("hero")[0]; // 히어로
 const cssOfHero = window.getComputedStyle(hero); // .hero의 css에 접근
 let leftValue = cssOfHero.left; // .here의 css 효과중 left의 값
 let leftValueWithoutPx = parseInt(leftValue); // .hero의 css중 left의 값에서 px제거한 숫자
 
-window.addEventListener('keydown', move)
+window.addEventListener("keydown", move);
 
-function move(e){
-    if((e.key === 'a'|| e.key ==='ㅁ') && hero.offsetLeft > 0){
-        hero.style.left = (hero.offsetLeft - 20)+'px';
-        hero.style.backgroundPosition = '70px';
-    } else if((e.key === 'd'||e.key ==='ㅇ') && hero.offsetLeft < 770){
-        hero.style.left = (hero.offsetLeft + 20)+'px';
-        hero.style.backgroundPosition = '35px';
-    } else if(e.key === 's' || e.key ==='ㄴ'){
-        hero.style.backgroundPosition = '0px'
-    } else if(e.key === 'w' || e.key === 'ㅈ'){ 
-         hero.style.backgroundPosition = '105px'
-    }
-}   
+function move(e) {
+  if ((e.key === "a" || e.key === "ㅁ") && hero.offsetLeft > 0) {
+    hero.style.left = hero.offsetLeft - 20 + "px";
+    hero.style.backgroundPosition = "70px";
+  } else if ((e.key === "d" || e.key === "ㅇ") && hero.offsetLeft < 770) {
+    hero.style.left = hero.offsetLeft + 20 + "px";
+    hero.style.backgroundPosition = "35px";
+  } else if (e.key === "s" || e.key === "ㄴ") {
+    hero.style.backgroundPosition = "0px";
+  } else if (e.key === "w" || e.key === "ㅈ") {
+    hero.style.backgroundPosition = "105px";
+  }
+}
 
 // function move(e){
 //         if(e.key === 'a' || e.key === 'ㅁ'){
@@ -30,7 +30,7 @@ function move(e){
 //             hero.style.left = toRight+'px';
 //         } else if(e.key === 's' || e.key ==='ㄴ'){
 //             hero.style.backgroundPosition = '0px'
-//         } else if(e.key === 'w' || e.key === 'ㅈ'){ 
+//         } else if(e.key === 'w' || e.key === 'ㅈ'){
 //             hero.style.backgroundPosition = '105px'
 //         }
 // }
